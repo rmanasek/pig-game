10429bfbfae705dac589a453350f988517bc8da4
Manasek-EXTERNAL Radek <rmanasek@gemalto.com>
2019-03-03 10:08:55 +0100

    initial revision

---
+++ www/README.md
@@ -0,0 +1,3 @@
+# pig game
+
+This is my study project where I would like to learn mainly javascript, but also html css and also PhoneGap.
---
+++ www/app.js
@@ -0,0 +1,87 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer;
+
+init();
+
+document.querySelector('.dice').style.display = 'none';
+
+[0,1].forEach(i=>{
+    document.getElementById(`score-${i}`).textContent = '0';
+    document.getElementById(`current-${i}`).textContent = '0';
+});
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    console.log(dice);
+    
+    // 2. display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = `dice-${dice}.png`;
+
+    // 3. Update the round score IF the rolled number was NOT a 1
+    if(dice !== 1) {
+        // add score
+        roundScore += dice;
+        document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+    } else {
+        nextPlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // add CURRENT score with the GLOBAL score
+    scores[activePlayer] += roundScore;
+    
+    // Update the UI
+    document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
+
+    // Check if the player won the game
+    if(scores[activePlayer]>=20) {
+        console.log(`Player ${activePlayer} won!`);
+        document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!';
+        document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+        document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+        document.querySelector('.dice').style.display = 'none';
+    } else {
+        // next player
+        nextPlayer();
+    }
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    [0,1].forEach(i=>{
+        document.getElementById(`current-${i}`).textContent = '0';
+    });
+    document.querySelector(`.player-0-panel`).classList.toggle('active');
+    document.querySelector(`.player-1-panel`).classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', function(){
+    init();
+});
+
+function init() {
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+}
+
+// document.querySelector(`#current-${activePlayer}`).textContent = dice;
+// document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`;
+
+// var x = document.querySelector(`#score-0`).textContent;
